b2b3d50e50f6191a89077c6f65ccfeb0c345fcbe
fast-tortoise <1rn18is121@gmail.com>
2024-05-04 12:44:43 +0530

    adding routes

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
-const SECRET = 'VarunKumarProject';
+const SECRET = 'VARUN-REST-API';
 
 export const random = () => crypto.randomBytes(128).toString('base64');
 export const authentication = (salt : string, password : string ) => {
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -27,3 +28,5 @@ const MONGODB_SERVER_URL = "mongodb+srv://varunkumarsde:HMume44IZ8zSclDA@cluster
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_SERVER_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use("/", router());
