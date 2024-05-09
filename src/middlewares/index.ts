92426b377b2396c8545d5adf4dd41fd0c5d44d52
fast-tortoise <1rn18is121@gmail.com>
2024-05-10 01:51:40 +0530

    user middleware  added

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,1 +1,28 @@
 import express from 'express';
+import {get, merge} from 'lodash';
+import { getUsersBySessionToken } from '../db/users';
+
+export const isAuththenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try
+    {
+        const sessionToken = req.cookies['VARUN-AUTH'];
+
+        if(!sessionToken){
+            return res.sendStatus(403);
+        }
+
+        const exsistingUser = await getUsersBySessionToken(sessionToken);
+        if(!exsistingUser){
+            return res.sendStatus(403);
+        }
+
+        merge(req, {identity: exsistingUser});
+        return next();
+
+    } catch(err){
+        console.log(err);
+        return res.sendStatus(400);
+    }
+}
+
+
